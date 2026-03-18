e85e274d017125ff5f2462f347494b4e89071f4b
femiayodeji <femidotayodeji@gmail.com>
2026-03-18 02:05:50 +0100

    Add recursive generation

--- src/main.py
+++ src/main.py
@@ -19,12 +19,25 @@ def generate_page(from_path, template_path, dest_path):
     template_content = template_content.replace("{{ Title }}", html_title)
     template_content = template_content.replace("{{ Content }}", html_string)
 
-    with open(dest_path, "w") as f:
-        if not os.path.exists(os.path.dirname(dest_path)):
-            os.makedirs(os.path.dirname(dest_path))
-        f.write(template_content)
-        f.close()
+    destination_parent = os.path.dirname(dest_path)
+    if destination_parent and not os.path.exists(destination_parent):
+        os.makedirs(destination_parent)
 
+    with open(dest_path, "w") as f:
+        f.write(template_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.makedirs(dest_dir_path)
+
+    for item in os.listdir(dir_path_content):
+        item_path_content = os.path.join(dir_path_content, item)
+        item_path_dest = os.path.join(dest_dir_path, item)
+
+        if os.path.isfile(item_path_content) and item.endswith(".md"):
+            generate_page(item_path_content, template_path, item_path_dest.replace(".md", ".html"))
+        elif os.path.isdir(item_path_content):
+            generate_pages_recursive(item_path_content, template_path, item_path_dest)
 
 def main():
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,11 +45,13 @@ def main():
     destination_dir = os.path.join(project_root, "public")
     copy_directory_recursively(source_dir, destination_dir)
 
-    generate_page(
-        os.path.join(project_root, "content", "index.md"),
-        os.path.join(project_root, "", "template.html"),
-        os.path.join(destination_dir, "index.html"),
+    generate_pages_recursive(
+        os.path.join(project_root, "content"), 
+        os.path.join(project_root, "template.html"), 
+        destination_dir
     )
+
+
 
 if __name__ == "__main__":
     main()
